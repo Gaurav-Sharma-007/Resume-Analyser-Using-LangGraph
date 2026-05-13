bb48b94ef7f5c557fc1a4fe616b1cd3362001303
Gaurav-Sharma-007 <myspacegaurav7@gmail.com>
2026-05-13 12:26:25 +0530

    Structuring

---
+++ app/agents/skill-agent.py
@@ -0,0 +1,16 @@
+def skill_analysis_node(state):
+
+    prompt = f"""
+    Extract technical and soft skills from:
+
+    {state['parsed_resume']}
+    """
+
+    response = llm.invoke(prompt)
+
+    skills = response.content.split(",")
+
+    state["skills"] = skills
+    state["current_step"] = "skills_analyzed"
+
+    return state
